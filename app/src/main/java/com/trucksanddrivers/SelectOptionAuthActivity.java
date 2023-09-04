713fa42d6f846ef6b4b975e67a504751351ee7a7
Lauren <lcvalentina3212401957@gmail.com>
2023-09-04 18:34:40 -0500

    Se agrega Funcionalidad a los Botones de iniciar sesión y registrese

--- app/src/main/java/com/trucksanddrivers/SelectOptionAuthActivity.java
+++ app/src/main/java/com/trucksanddrivers/SelectOptionAuthActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 public class SelectOptionAuthActivity extends AppCompatActivity {
 
     Button mButtonIniciar;
-
+    Button mButtonRegistrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,17 +18,33 @@ public class SelectOptionAuthActivity extends AppCompatActivity {
 
         //Obtiene referencia al Botón en el layout
         mButtonIniciar = findViewById(R.id.btnIniSes);
+        // Obtén referencia al Botón de Registrarse en el layout
+        mButtonRegistrar = findViewById(R.id.btnRegis);
 
         //Configura el evento de click para el Botón
         mButtonIniciar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { goToRegisterActivity();
+            public void onClick(View v) { goToLoginActivity();
             //Aqui es donde se muestra como abrir otra actividad o realizar alguna operación
                 //Tambien como abrir el RegisterActivity
-            }
+             }
         });
-
+    // Configura el evento de click para el Botón de Registrarse
+        mButtonRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Aqui es donde se muestra como abrir otra actividad o realizar alguna operación
+                //Tambien como abrir el RegisterActivity
+                goToRegisterActivity();
+                }
+        });
     }
+
+    private void goToLoginActivity() {
+        Intent intent = new Intent(SelectOptionAuthActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
     private void goToRegisterActivity() {
         Intent intent = new Intent(SelectOptionAuthActivity.this, RegisterActivity.class);
         startActivity(intent);
